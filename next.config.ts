3bcef17eed55657e6cfd2bb5f5107b46d1712a85
Binh Duy <tbduy.dev@gmail.com>
2026-01-23 22:45:02 +0700

    fix: add unpkg.com to image domains for leaflet icons

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,74 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "bit.ly",
+      },
+      {
+        protocol: "https",
+        hostname: "cf.bstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "vinwonders.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn3.ivivu.com",
+      },
+      {
+        protocol: "https",
+        hostname: "statics.vinpearl.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "nhatrangtourist.com.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "znews-photo.zingcdn.me",
+      },
+      {
+        protocol: "https",
+        hostname: "vtv1.mediacdn.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.vntrip.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "media.vneconomy.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "vcdn1-dulich.vnecdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "khanhhoatrip.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.nttravel.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "sunworld.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "unpkg.com",
+      },
     ],
   },
 };
